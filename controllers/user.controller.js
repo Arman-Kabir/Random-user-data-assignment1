e4896455ea620a8d2bb9c911f953061957a76b69
Arman-Kabir <armankabir8@gmail.com>
2022-09-03 20:57:59 +0600

    bulk-update done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,7 +57,56 @@ module.exports.saveRandomUser = (req, res, next) => {
 }
 
 
+module.exports.updateAUser = (req, res, next) => {
+    const newData = req.body;
+    fs.readFile("userData.json", "utf-8", (err, data) => {
+        const loadedData = JSON.parse(data);
+        const isValid = loadedData.find(data => data.id == newData.id);
+        if (isValid) {
+            const allData = loadedData.filter(data => data.id != newData.id);
+            let updatedData = [...allData, newData];
+            const savedData = JSON.stringify(updatedData);
+            fs.writeFile("userData.json", savedData, function (err) {
+                console.log(`updated id ${newData.id} and saved`);
+                res.send(savedData);
+            });
+        } else {
+            res.send(`this id: ${newData.id} is not available`);
+        }
+    });
+}
 
+module.exports.updateMultipleUser = (req, res, next) => {
+    const newData = req.body;
+    // res.send(newData);
+
+    fs.readFile("userData.json", "utf-8", (err, data) => {
+        const loadedData = JSON.parse(data);
+        let isValid;
+        let availableData = [];
+        let notAvailableData = 0;
+        for (let i = 0; i < newData.length; i++) {
+            console.log(i);
+            isValid = loadedData.find(data => data.id == newData[i].id);
+            // console.log(isValid);
+
+            if (isValid) {
+                const allData = loadedData.filter(data => data.id != Number(isValid.id));
+                console.log(allData);
+                let updatedData = [...allData, newData[i]];
+                const savedData = JSON.stringify(updatedData);
+                fs.writeFile("userData.json", savedData, function (err) {
+                    console.log(`updated id  and saved`);
+                    // res.send(savedData);
+                });
+            } else {
+                console.log(`id: ${newData[i].id} not available`);
+                notAvailableData++;
+            }
+        }
+        res.send(`updated= available ids and not updated ${notAvailableData} id cz not available in the DB `);
+    });
+}
 
 
 module.exports.deleteUser = (req, res, next) => {
@@ -65,16 +114,17 @@ module.exports.deleteUser = (req, res, next) => {
     console.log(id);
     fs.readFile("userData.json", "utf-8", (err, data) => {
         const loadedData = JSON.parse(data);
-        const updatedData = loadedData.filter(data => data.id != id);
-        const savedData = JSON.stringify(updatedData);
-        // console.log(updatedData);
-        // console.log(loadedData);         
-        // console.log(typeof(loadedData));         
-        // console.log(typeof(newData)); 
-        res.send(savedData);
+        const isValid = loadedData.find(data => data.id == id);
 
-        fs.writeFile("userData.json", savedData, function (err) {
-            console.log('saved');
-        });
+        if (isValid) {
+            const updatedData = loadedData.filter(data => data.id != id);
+            const savedData = JSON.stringify(updatedData);
+            fs.writeFile("userData.json", savedData, function (err) {
+                console.log(`deleted id ${id} and saved`);
+                res.send(savedData);
+            });
+        } else {
+            res.send(`this id: ${id} is not available`);
+        }
     });
 }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.route("/random").get(userController.aRandomUser);
 router.route("/all").get(userController.allRandomUsers);
 router.route("/save").post(userController.saveRandomUser);
+router.route("/update").patch(userController.updateAUser);
+router.route("/bulk-update").patch(userController.updateMultipleUser);
 
 
 router.route("/delete/:id").delete(userController.deleteUser);
